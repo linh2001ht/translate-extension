046933792b1658d06111bf6ad34c7998ed5b0305
linh2001ht <linh2001ht@gmail.com>
2024-02-02 13:27:17 +0700

    init

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-as5im1-8vs_tc^dp-01n6m5mnm^g)4ct6256r0r%l=!n+ks(b$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app',
 ]
 
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
